d6c51d9bf5d6fa33eade6ad9a12834902c2405c3
rustielin <rustielin@berkeley.edu>
2017-07-16 18:05:53 -0700

    refactor launch

--- core/src/com/rustie/game/Slit.java
+++ core/src/com/rustie/game/Slit.java
@@ -11,41 +11,42 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.rustie.game.screens.GameScreenManager;
 import com.rustie.game.screens.MenuScreen;
-import com.rustie.game.screens.PlayScreen;
-//import com.rustie.game.states.GameStateManager;
-//import com.rustie.game.states.MenuState;
 
 public class Slit extends Game {
+
     // virtual width and heights
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
 
-    // pixels per meter
+    // pixels per meter for scaling purposes
     public static final float PPM = 100;
 
+	// some useful game attributes
 	public static final String TITLE = "Slit";
     public static Application.ApplicationType APP_TYPE;
 	public static boolean IS_MOBILE;
 
+	// for filters, collisions, lighting, etc
 	public static final short DEFAULT_BIT = 1;
 	public static final short PLAYER_BIT = 2;
 	public static final short COIN_BIT = 4;
 	public static final short WALL_BIT = 8;
 	public static final short DESTROYED_BIT = 16;
 
+	//
 	public static Integer SCORE = 0;
 
+    // stack that keeps track of which screen we're on
 	private GameScreenManager mGsm;
 
-	// only need 1
+	// only need 1 per game instance to save memory
 	public static SpriteBatch mBatch;
-
-	Texture img;
 
 	@Override
 	public void create() {
 		this.mBatch = new SpriteBatch();
 
+        // debug
 //		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
 		APP_TYPE = Gdx.app.getType();
@@ -54,9 +55,6 @@ public class Slit extends Game {
 
 		this.mGsm = new GameScreenManager(this);
 		this.mGsm.push(new MenuScreen(mGsm, this));
-
-//		setScreen(new PlayScreen(mGsm, this, "level1.tmx"));
-
 	}
 
 	@Override
@@ -76,7 +74,6 @@ public class Slit extends Game {
 	@Override
 	public void dispose() {
 		mBatch.dispose();
-		img.dispose();
 	}
 
 
